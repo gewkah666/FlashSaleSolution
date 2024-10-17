625e48e44828bfe7939180f1a69b6bfeda50f93d
Weikang Ge <weikang.ge@saksoff5th.com>
2024-10-17 23:19:29 +0800

    init

---
+++ src/main/java/cn/garry/flashsalesolution/FlashSaleSolutionApplication.java
@@ -0,0 +1,13 @@
+package cn.garry.flashsalesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashSaleSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/garry/flashsalesolution/FlashSaleSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.garry.flashsalesolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashSaleSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
